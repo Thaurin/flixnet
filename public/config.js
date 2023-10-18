dc5f77c4149a97b1ae1fdd6d0a277810f6e3b715
Ivo van Heel <thaurin@gmail.com>
2023-10-18 16:56:43 +0200

    Put correct settings in config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://flixnet.thaurin.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YzIyNjExODEzZDEwYmU2MDQ5ZTc2NDMxNzdkYTI5MiIsInN1YiI6IjY1MmZmMDUzY2FlZjJkMDBmZjU0NGU2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Knl2qiEgjCNaTTmV1vK0aEO16qf-p7J2ddIEnJKzitY"
 };
